82fcdfb935107d3b5a588dbe15dfe2cfdc443576
www.linkedin.com/in/artur-ulaszek <40719066+Artludwik@users.noreply.github.com>
2018-07-14 16:54:39 +0200

    Add files via upload

---
+++ First Steps.100yearsold.py
@@ -0,0 +1,11 @@
+#My first Project
+
+#Returns the username
+name = input("Give me your name: ")
+print("Your Name is " + name)
+#The Program returns a information "Your Name is + name"
+age = int(input("Please give me your age / how old are you?: "))
+print("You are " + str(age) + " years old")
+#Returns the date after 100 years
+year = str((2018 - age) + 100)
+print(name + " will be 100 years old in " + year)
