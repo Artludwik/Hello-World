9cba398785c928bafb2d2fab4c764cc61b36bb40
www.linkedin.com/in/artur-ulaszek <40719066+Artludwik@users.noreply.github.com>
2018-07-14 14:22:06 +0200

    Update First Steps.givemeyourname.py

--- First Steps.givemeyourname.py
+++ First Steps.givemeyourname.py
@@ -1,4 +1,4 @@
 #My First project
 name = input("Give me your name: ")
 print("Your Name is " + name)
-#The Pogram bring back a quate "Your Name is + name"
+#The Pogram bring back a quote "Your Name is + name"
